08c50443d5d7c79ee152a42ef9371b8991b67a14
Brandi Burnell <brandiburnell6@gmail.com>
2024-02-22 14:02:49 -0700

    pass test

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,16 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    result = {}
+    nodes_to_visit = [self.root]
+
+    while len(nodes_to_visit) > 0:
+      node = nodes_to_visit.pop(0)
+      if node['id'] == id:
+        result.update(node)
+      nodes_to_visit = nodes_to_visit + node['children']
+    
+    if len(result) == 0:
+      return None
+    return result
+    
